6e7c9a596243e2e4e28492162d7adc4b8c4b90b5
Study Buffalo <joshua@torrance.io>
2024-04-01 18:58:03 -0600

    Added all new scenarios

--- simulation/scenarios/__init__.py
+++ simulation/scenarios/__init__.py
@@ -1,7 +1,14 @@
 """Module to hold all the different scenarios for testing."""
-from .combined_teams import combined_teams
-from .current import current_state
+from .combined_teams import scenario as scenario_combined
+from .current import scenario as scenario_current
+from .no_im import scenario as scenario_no_im
+from .status_quo import scenario as scenario_status_quo
 from .utils import cycle_length, Stats
 
-scenarios = [current_state, combined_teams]
+scenarios = [
+    scenario_current, 
+    scenario_status_quo,
+    scenario_combined, 
+    scenario_no_im,
+]
 cycle_length = cycle_length
