05600f72ff29abe5aedf2e7eb75847d36b33a5fb
Study Buffalo <joshua@torrance.io>
2024-04-02 20:56:05 -0600

    Adding code to output results into an Excel document

--- simulation/__main__.py
+++ simulation/__main__.py
@@ -1,4 +1,5 @@
 """Runs the simulations."""
+from pathlib import Path
 import time
 
 import numpy as np
@@ -188,11 +189,11 @@ for scenario in scenarios:
 
     # Iterate through each scenario results
     for cycle_results in scenario_results:
-        all_events_0 = 0
-        all_events_2 = 0
-        all_events_4 = 0
-        all_events_12 = 0
-        all_events_total = 0
+        all_events_0 = []
+        all_events_2 = []
+        all_events_4 = []
+        all_events_12 = []
+        all_events_total = []
         all_uncovered_shifts = 0
 
         # Iterate through the results of each event
@@ -209,11 +210,11 @@ for scenario in scenarios:
             simulation_event_results[name]['outcome_12'].append(outcome_12)
             simulation_event_results[name]['outcome_total'].append(outcome_total)
 
-            all_events_0 += outcome_0
-            all_events_2 += outcome_2
-            all_events_4 += outcome_4
-            all_events_12 += outcome_12
-            all_events_total += outcome_total
+            all_events_0.append(outcome_0)
+            all_events_2.append(outcome_2)
+            all_events_4.append(outcome_4)
+            all_events_12.append(outcome_12)
+            all_events_total.append(outcome_total)
         
         # Update the special "All Events" entry
         simulation_event_results['All Events']['outcome_0'] = all_events_0
@@ -277,9 +278,192 @@ for scenario in scenarios:
     shift_changes_stats_12 = Stats(simulation_shift_changes['week_12'])
     shift_changes_stats_total = Stats(simulation_shift_changes['week_total'])
 
+    # Write data to the active worksheet
+    row_num = 1
+
+    # Basic Monte Carlo Details
+    output_ws.cell(row=row_num, column=1, value='SIMULATION DETAILS')
+    row_num += 1
+    
+    output_ws.cell(row=row_num, column=2, value='Value')
+    row_num += 1
+
+    output_ws.cell(row=row_num, column=1, value='Number of Simulations')
+    output_ws.cell(row=row_num, column=2, value=num_simulations)
+    row_num += 1
+
+    output_ws.cell(row=row_num, column=1, value='Length of Simulation Cycle (weeks)')
+    output_ws.cell(row=row_num, column=2, value=cycle_length)
+    row_num += 2
+
+    # Event Details
+    output_ws.cell(row=row_num, column=1, value='EVENT DETAILS')
+    row_num += 1
+
+    output_ws.cell(row=row_num, column=1, value='Event')
+    output_ws.cell(row=row_num, column=2, value='Rate of Changes (changes per event occurrence)')
+    output_ws.cell(row=row_num, column=3, value='Rate of Lost Shift Capacity (lost shifts per event occurrence)')
+    output_ws.cell(row=row_num, column=4, value='Event Rate Occurence - Total')
+    output_ws.cell(row=row_num, column=5, value='Event Rate Occurence - 0 to 2 weeks')
+    output_ws.cell(row=row_num, column=6, value='Event Rate Occurence - 2 to 4 weeks')
+    output_ws.cell(row=row_num, column=7, value='Event Rate Occurence - 4 to 12 weeks')
+    output_ws.cell(row=row_num, column=8, value='Event Rate Occurence - 12+ weeks')
+    output_ws.cell(row=row_num, column=9, value='Maximum Number of Allowed Events per Cycle')
+    row_num += 1
+
+    for event in scenario.events:
+        output_ws.cell(row=row_num, column=1, value=event.name)
+        output_ws.cell(row=row_num, column=2, value=event.changes)
+        output_ws.cell(row=row_num, column=3, value=event.losses)
+        output_ws.cell(row=row_num, column=4, value=np.round(event.rate_total, 2))
+        output_ws.cell(row=row_num, column=5, value=np.round(event.rate_0, 2))
+        output_ws.cell(row=row_num, column=6, value=np.round(event.rate_2, 2))
+        output_ws.cell(row=row_num, column=7, value=np.round(event.rate_4, 2))
+        output_ws.cell(row=row_num, column=8, value=np.round(event.rate_12, 2))
+        output_ws.cell(row=row_num, column=9, value=event.cycle_max)
+        row_num +=1
+
+    row_num +=1
+
+    # Event occurence details for the simulation
+    output_ws.cell(row=row_num, column=1, value='EVENT RESULTS')
+    row_num += 1
+
+    output_ws.cell(row=row_num, column=1, value='Event')
+    output_ws.cell(row=row_num, column=2, value='Total - Mean')
+    output_ws.cell(row=row_num, column=3, value='Total - Lower CI')
+    output_ws.cell(row=row_num, column=4, value='Total - Upper CI')
+    output_ws.cell(row=row_num, column=5, value='Weeks 0 to 2 - Mean')
+    output_ws.cell(row=row_num, column=6, value='Weeks 0 to 2 - Lower CI')
+    output_ws.cell(row=row_num, column=7, value='Weeks 0 to 2 - Upper CI')
+    output_ws.cell(row=row_num, column=8, value='Weeks 2 to 4 - Mean')
+    output_ws.cell(row=row_num, column=9, value='Weeks 2 to 4 - Lower CI')
+    output_ws.cell(row=row_num, column=10, value='Weeks 2 to 4 - Upper CI')
+    output_ws.cell(row=row_num, column=11, value='Weeks 4 to 12 - Mean')
+    output_ws.cell(row=row_num, column=12, value='Weeks 4 to 12 - Lower CI')
+    output_ws.cell(row=row_num, column=13, value='Weeks 4 to 12 - Upper CI')
+    output_ws.cell(row=row_num, column=14, value='Weeks 12+ - Mean')
+    output_ws.cell(row=row_num, column=15, value='Weeks 12+ - Lower CI')
+    output_ws.cell(row=row_num, column=16, value='Weeks 12+ - Upper CI')
+
+    row_num += 1
+
+    for event_name, event_stats in simulations_stats['events'].items():
+        output_ws.cell(row=row_num, column=1, value=event_name)
+        output_ws.cell(row=row_num, column=2, value=event_stats['stats_total'].mean)
+        output_ws.cell(row=row_num, column=3, value=event_stats['stats_total'].ci_lower)
+        output_ws.cell(row=row_num, column=4, value=event_stats['stats_total'].ci_upper)
+        output_ws.cell(row=row_num, column=5, value=event_stats['stats_0'].mean)
+        output_ws.cell(row=row_num, column=6, value=event_stats['stats_0'].ci_lower)
+        output_ws.cell(row=row_num, column=7, value=event_stats['stats_0'].ci_upper)
+        output_ws.cell(row=row_num, column=8, value=event_stats['stats_2'].mean)
+        output_ws.cell(row=row_num, column=9, value=event_stats['stats_2'].ci_lower)
+        output_ws.cell(row=row_num, column=10, value=event_stats['stats_2'].ci_upper)
+        output_ws.cell(row=row_num, column=11, value=event_stats['stats_4'].mean)
+        output_ws.cell(row=row_num, column=12, value=event_stats['stats_4'].ci_lower)
+        output_ws.cell(row=row_num, column=13, value=event_stats['stats_4'].ci_upper)
+        output_ws.cell(row=row_num, column=14, value=event_stats['stats_12'].mean)
+        output_ws.cell(row=row_num, column=15, value=event_stats['stats_12'].ci_lower)
+        output_ws.cell(row=row_num, column=16, value=event_stats['stats_12'].ci_upper)
+        row_num += 1
+
+    row_num += 1
+
+    # Shift Details
+    output_ws.cell(row=row_num, column=1, value='SHIFT DETAILS')
+    row_num += 1
+
+    output_ws.cell(row=row_num, column=1, value='Shift Name')
+    output_ws.cell(row=row_num, column=2, value='Number of Shifts')
+    output_ws.cell(row=row_num, column=3, value='Shift Priority')
+    row_num += 1
+
+    for shift in scenario.shifts:
+        output_ws.cell(row=row_num, column=1, value=shift.name)
+        output_ws.cell(row=row_num, column=2, value=shift.number)
+        output_ws.cell(row=row_num, column=3, value=shift.priority)
+        row_num += 1
+        
+    row_num += 1
+
+    # Shift Results
+    output_ws.cell(row=row_num, column=1, value='UNCOVERED SHIFT RESULTS')
+    row_num += 1
+
+    output_ws.cell(row=row_num, column=1, value='Shift')
+    output_ws.cell(row=row_num, column=2, value='Mean Uncovered Shifts per Cycle')
+    output_ws.cell(row=row_num, column=3, value='Lower CI')
+    output_ws.cell(row=row_num, column=4, value='Upper CI')
+    row_num += 1
+
+    for shift_name, shift_stats in simulations_stats['uncovered_shifts'].items():
+        output_ws.cell(row=row_num, column=1, value=shift_name)
+        output_ws.cell(row=row_num, column=2, value=shift_stats.mean)
+        output_ws.cell(row=row_num, column=3, value=shift_stats.ci_lower)
+        output_ws.cell(row=row_num, column=4, value=shift_stats.ci_upper)
+        row_num += 1
+
+    row_num += 1
+
+    # Excess Shift Results
+    output_ws.cell(row=row_num, column=1, value='EXCESS SHIFT RESULTS')
+    row_num += 1
+    
+    output_ws.cell(row=row_num, column=2, value='Mean Number of Shifts Per Cycle')
+    output_ws.cell(row=row_num, column=3, value='Lower CI')
+    output_ws.cell(row=row_num, column=4, value='Upper CI')
+    row_num += 1
+    
+    output_ws.cell(row=row_num, column=1, value='Number of Excess Shifts')
+    output_ws.cell(row=row_num, column=2, value=excess_shifts_stats.mean)
+    output_ws.cell(row=row_num, column=3, value=excess_shifts_stats.ci_lower)
+    output_ws.cell(row=row_num, column=4, value=excess_shifts_stats.ci_upper)
+    row_num += 2
+
+    # Number of Shift Changes
+    output_ws.cell(row=row_num, column=1, value='NUMBER OF SHIFT CHANGES')
+    row_num += 1
+
+    output_ws.cell(row=row_num, column=2, value='Total - Mean')
+    output_ws.cell(row=row_num, column=3, value='Total - Lower CI')
+    output_ws.cell(row=row_num, column=4, value='Total - Upper CI')
+    output_ws.cell(row=row_num, column=5, value='Weeks 0 to 2 - Mean')
+    output_ws.cell(row=row_num, column=6, value='Weeks 0 to 2 - Lower CI')
+    output_ws.cell(row=row_num, column=7, value='Weeks 0 to 2 - Upper CI')
+    output_ws.cell(row=row_num, column=8, value='Weeks 2 to 4 - Mean')
+    output_ws.cell(row=row_num, column=9, value='Weeks 2 to 4 - Lower CI')
+    output_ws.cell(row=row_num, column=10, value='Weeks 2 to 4 - Upper CI')
+    output_ws.cell(row=row_num, column=11, value='Weeks 4 to 12 - Mean')
+    output_ws.cell(row=row_num, column=12, value='Weeks 4 to 12 - Lower CI')
+    output_ws.cell(row=row_num, column=13, value='Weeks 4 to 12 - Upper CI')
+    output_ws.cell(row=row_num, column=14, value='Weeks 12+ - Mean')
+    output_ws.cell(row=row_num, column=15, value='Weeks 12+ - Lower CI')
+    output_ws.cell(row=row_num, column=16, value='Weeks 12+ - Upper CI')
+    row_num += 1
+
+    output_ws.cell(row=row_num, column=1, value='Number of Shift Changes')
+    output_ws.cell(row=row_num, column=2, value=shift_changes_stats_total.mean)
+    output_ws.cell(row=row_num, column=3, value=shift_changes_stats_total.ci_lower)
+    output_ws.cell(row=row_num, column=4, value=shift_changes_stats_total.ci_upper)
+    output_ws.cell(row=row_num, column=5, value=shift_changes_stats_0.mean)
+    output_ws.cell(row=row_num, column=6, value=shift_changes_stats_0.ci_lower)
+    output_ws.cell(row=row_num, column=7, value=shift_changes_stats_0.ci_upper)
+    output_ws.cell(row=row_num, column=8, value=shift_changes_stats_2.mean)
+    output_ws.cell(row=row_num, column=9, value=shift_changes_stats_2.ci_lower)
+    output_ws.cell(row=row_num, column=10, value=shift_changes_stats_2.ci_upper)
+    output_ws.cell(row=row_num, column=11, value=shift_changes_stats_4.mean)
+    output_ws.cell(row=row_num, column=12, value=shift_changes_stats_4.ci_lower)
+    output_ws.cell(row=row_num, column=13, value=shift_changes_stats_4.ci_upper)
+    output_ws.cell(row=row_num, column=14, value=shift_changes_stats_12.mean)
+    output_ws.cell(row=row_num, column=15, value=shift_changes_stats_12.ci_lower)
+    output_ws.cell(row=row_num, column=16, value=shift_changes_stats_12.ci_upper)
+    
     # Create a new worksheet if necessary
-    if len(output_wb.worksheets) < num_simulations:
+    if len(output_wb.worksheets) < num_scenarios:
         output_ws = output_wb.create_sheet()
 
 # Save the workbook results
-output_wb.save(f'simulation_results_{int(time.time())}.xlsx')
+current_loc = Path('.')
+save_loc = (current_loc / 'results' / f'simulation_results_{int(time.time())}.xlsx').resolve()
+print(f'Writing results to file: {save_loc}')
+output_wb.save(save_loc)
